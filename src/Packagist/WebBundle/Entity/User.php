e5f71d097cf8b5f194828e4ee754e72421bd2b7e
xaav <xaav@xaav.tk>
2011-06-29 10:29:20 -0500

    Ignored files.

---
+++ src/Packagist/WebBundle/Entity/User.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Packagist\WebBundle\Entity;
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\generatedValue(strategy="AUTO")
+     */
+    protected $id;
+}
