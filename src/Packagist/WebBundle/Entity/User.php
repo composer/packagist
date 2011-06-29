aef60516cc3c37dd260441f3ed1869186a805ab9
xaav <xaav@xaav.tk>
2011-06-29 11:57:34 -0500

    Modified entities.

--- src/Packagist/WebBundle/Entity/User.php
+++ src/Packagist/WebBundle/Entity/User.php
@@ -17,4 +17,43 @@ class User extends BaseUser
      * @ORM\generatedValue(strategy="AUTO")
      */
     protected $id;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="Package", mappedBy="maintainers")
+     */
+    private $packages;
+    public function __construct()
+    {
+        $this->packages = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+    
+    /**
+     * Get id
+     *
+     * @return integer $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Add packages
+     *
+     * @param Packagist\WebBundle\Entity\Package $packages
+     */
+    public function addPackages(\Packagist\WebBundle\Entity\Package $packages)
+    {
+        $this->packages[] = $packages;
+    }
+
+    /**
+     * Get packages
+     *
+     * @return Doctrine\Common\Collections\Collection $packages
+     */
+    public function getPackages()
+    {
+        return $this->packages;
+    }
 }
