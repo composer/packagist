45a5230af5d48c00b8e4bc4c216c9c85049b6153
xaav <xaav@xaav.tk>
2011-06-30 08:08:04 -0700

    Edited app/AppCache.php via GitHub

--- app/AppCache.php
+++ app/AppCache.php
@@ -6,4 +6,10 @@ use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
 
 class AppCache extends HttpCache
 {
+    protected function forward(Request $request, $raw = false, Response $entry = null)
+    {
+        $this->getKernel()->loadClassCache();
+
+        return parent::forward($request, $raw, $entry);
+    }
 }
