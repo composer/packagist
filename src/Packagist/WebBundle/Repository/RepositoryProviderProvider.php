6f535005e224c927fab6937ccf69896aff82c08f
xaav <xaav@xaav.tk>
2011-07-07 12:52:09 -0500

    Added RepositoryProviderProvider.

---
+++ src/Packagist/WebBundle/Repository/RepositoryProviderProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Packagist\WebBundle\Repository;
+
+use Packagist\WebBundle\Repository\Provider\RepositoryProviderInterface;
+
+class RepositoryProviderProvider
+{
+    protected $providers = array();
+
+    public function addProvider(RepositoryProviderInterface $provider)
+    {
+        $this->providers[] = $provider;
+    }
+
+    public function getRepository($url)
+    {
+        foreach ($this->providers as $provider)
+        {
+            if($provider->supports($url))
+            {
+                return $provider->getRepository($url);
+            }
+        }
+    }
+}
