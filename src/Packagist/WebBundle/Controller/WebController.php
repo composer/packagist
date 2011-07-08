660bae40f91f3e7c534f1b6d67c4730d0a85f89e
xaav <xaav@xaav.tk>
2011-07-08 15:15:43 -0500

    Removed confirm action.

--- src/Packagist/WebBundle/Controller/WebController.php
+++ src/Packagist/WebBundle/Controller/WebController.php
@@ -57,7 +57,7 @@ class WebController extends Controller
         $package = new Package;
         $form = $this->get('form.factory')->create(new PackageType, $package);
 
-        $request = $this->get('request');
+        $request = $this->getRequest();
         $provider = $this->get('packagist.repository_provider');
         if ($request->getMethod() == 'POST') {
             $form->bindRequest($request);
@@ -69,39 +69,15 @@ class WebController extends Controller
                 $composerFile = $repository->getComposerInformation('master');
                 $package->setName($composerFile['name']);
 
-                $this->get('session')->set('package', $package);
-                return new RedirectResponse($this->generateUrl('confirm'));
+                $em = $this->getDoctrine()->getEntityManager();
+                $em->persist($package);
+                $em->flush();
+
+                return new RedirectResponse($this->generateUrl('home'));
             }
         }
 
         return array('form' => $form->createView(), 'page' => 'submit');
-    }
-
-    /**
-     * @Template()
-     * @Route("/submit/confirm", name="confirm")
-     */
-    public function confirmPackageAction()
-    {
-        if(($package = $this->get('session')->get('package')) instanceof Package) {
-            $confirmForm = new ConfirmForm;
-            $form = $this->createForm(new ConfirmFormType, $confirmForm);
-            $request = $this->getRequest();
-            if($request->getMethod() == 'POST') {
-                $form->bindRequest($request);
-                if($form->isValid()){
-                    try {
-                        $this->get('session')->remove('package');
-                        $this->getDoctrine()->getEntityManager()->persist($package);
-                        $this->get('session')->setFlash('success', $package->getName().' has been added to the package list, the repository will be parsed for releases in a bit.');
-                        return new RedirectResponse($this->generateUrl('home'));
-                    } catch (\PDOException $e) {
-                        $this->get('session')->setFlash('error', $package->getName().' could not be saved in our database, most likely the name is already in use.');
-                    }
-                }
-            }
-        }
-        return array('form' => $form->createView(), 'page' => 'confirm');
     }
 
     /**
--- src/Packagist/WebBundle/Form/ConfirmForm.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Packagist\WebBundle\Form;
-
-class ConfirmForm
-{
-}
