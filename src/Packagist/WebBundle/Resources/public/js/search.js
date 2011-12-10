0afd90fc3cf3c5a68fea654052eb0bf635afbe47
Igor Wiedler <igor@wiedler.ch>
2011-12-11 00:39:49 +0100

    improve search.js readability, handle search event (webkit search clear button)

--- src/Packagist/WebBundle/Resources/public/js/search.js
+++ src/Packagist/WebBundle/Resources/public/js/search.js
@@ -1,38 +1,39 @@
 (function ($) {
-    var doSearch,
+    var form = $('form#search-form'),
+        showResults,
+        doSearch,
         searching = false,
         searchQueued = false;
 
+    showResults = function (page) {
+        var list = $('.package-list'),
+            newList = $(page);
+
+        if (newList.find('.packages li').length) {
+            list.replaceWith(newList);
+            list.show();
+        } else {
+            list.hide();
+        }
+
+        searching = false;
+
+        if (searchQueued) {
+            doSearch();
+            searchQueued = false;
+        }
+    };
+
     doSearch = function () {
-        var form = $('form#search-form');
+        if (searching) {
+            searchQueued = true;
+            return;
+        }
 
-        $.get(form.attr('action'), form.serialize(), function (page) {
-            var list = $('.package-list'),
-                newList = $(page);
-
-            if (newList.find('.packages li').length) {
-                list.replaceWith(newList);
-                list.show();
-            } else {
-                list.hide();
-            }
-
-            searching = false;
-
-            if (searchQueued) {
-                doSearch();
-                searchQueued = false;
-            }
-        });
+        $.get(form.attr('action'), form.serialize(), showResults);
 
         searching = true;
     };
 
-    $('form#search-form').keyup(function (event) {
-        if (searching) {
-            searchQueued = true;
-        } else {
-            doSearch();
-        }
-    });
+    form.bind('keyup search', doSearch);
 })(jQuery);
