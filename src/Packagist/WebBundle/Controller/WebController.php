85a2349546457d09a1c46c4f80ab65ce5853a8e3
Robert Gruendler <r.gruendler@gmail.com>
2012-09-25 22:24:29 +0200

    added type to solr index and added type query-param for search action

--- src/Packagist/WebBundle/Controller/WebController.php
+++ src/Packagist/WebBundle/Controller/WebController.php
@@ -129,9 +129,16 @@ class WebController extends Controller
                 $solarium = $this->get('solarium.client');
 
                 $select = $solarium->createSelect();
-
                 $escapedQuery = $select->getHelper()->escapeTerm($form->getData()->getQuery());
-
+                $typeFilter = $req->get('type');
+                
+                // filter by type
+                if ($typeFilter !== null) {
+                	$filterQueryTerm = sprintf('type:%s', $select->getHelper()->escapeTerm($typeFilter));
+                	$filterQuery = $select->createFilterQuery('type')->setQuery($filterQueryTerm);
+                	$select->addFilterQuery($filterQuery);
+                }
+                
                 $dismax = $select->getDisMax();
                 $dismax->setQueryFields(array('name^2', 'description', 'tags', 'text', 'text_ngram', 'name_split^1.5'));
                 $dismax->setPhraseFields(array('description^30'));
