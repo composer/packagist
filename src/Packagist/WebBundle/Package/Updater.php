ced84c36d8de0ab83ba6df22e29975a1a820970c
Jordi Boggiano <j.boggiano@seld.be>
2013-04-07 11:48:26 +0200

    Fix duplicate keyword parsing

--- src/Packagist/WebBundle/Package/Updater.php
+++ src/Packagist/WebBundle/Package/Updater.php
@@ -227,7 +227,11 @@ class Updater
 
         $version->getTags()->clear();
         if ($data->getKeywords()) {
-            foreach (array_unique($data->getKeywords()) as $keyword) {
+            $keywords = array();
+            foreach ($data->getKeywords() as $keyword) {
+                $keywords[mb_strtolower($keyword, 'UTF-8')] = $keyword;
+            }
+            foreach ($keywords as $keyword) {
                 $tag = Tag::getByName($em, $keyword, true);
                 if (!$version->getTags()->contains($tag)) {
                     $version->addTag($tag);
