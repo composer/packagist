1c142b9b86ec421418d6a2dcae89bea92dd6511a
Dave Shoreman <codeM0nK3Y@me.com>
2013-10-29 14:06:41 +0000

    Remove password fields when logged in with Github
    If a user has used Github authentication to create their account,
    they are not able to update their password as one does not exist.
    This resolves the issue by removing the Change Password form, and
    also the Password input on the Edit Profile page.
    
    One possible improvement that could be made in future would be
    additional checks to ensure the user is still authenticated with
    Github, though I've spent all day on this already so I'll leave
    that to somebody else to add in ;)

--- src/Packagist/WebBundle/Form/Type/ProfileFormType.php
+++ src/Packagist/WebBundle/Form/Type/ProfileFormType.php
@@ -14,18 +14,60 @@ namespace Packagist\WebBundle\Form\Type;
 
 use FOS\UserBundle\Form\Type\ProfileFormType as BaseType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\Security\Core\Validator\Constraint\UserPassword as OldUserPassword;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
 
 /**
  * @author Jordi Boggiano <j.boggiano@seld.be>
  */
 class ProfileFormType extends BaseType
 {
+    private $securityContext;
+
+    public function __construct($class, SecurityContext $securityContext)
+    {
+        parent::__construct($class);
+
+        $this->securityContext = $securityContext;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        parent::buildForm($builder, $options);
+        $builder->add('username', null, array('label' => 'form.username', 'translation_domain' => 'FOSUserBundle'))
+                ->add('email', 'email', array('label' => 'form.email', 'translation_domain' => 'FOSUserBundle'));
+
+        $user = $this->securityContext->getToken()->getUser();
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $event) use ($user)
+        {
+            if ( ! $user->getGithubId())
+            {
+                $this->addPasswordField($event);
+            }
+        });
 
         $builder->add('failureNotifications', null, array(
             'required' => false,
+        ));
+    }
+
+    private function addPasswordField(&$event)
+    {
+        if (class_exists('Symfony\Component\Security\Core\Validator\Constraints\UserPassword')) {
+            $constraint = new UserPassword();
+        } else {
+            // Symfony 2.1 support with the old constraint class
+            $constraint = new OldUserPassword();
+        }
+
+        $event->getForm()->add('current_password', 'password', array(
+            'label' => 'form.current_password',
+            'translation_domain' => 'FOSUserBundle',
+            'mapped' => false,
+            'constraints' => $constraint,
         ));
     }
 
