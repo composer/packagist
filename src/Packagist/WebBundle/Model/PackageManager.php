d0ec01174a9a011a99862cb4c5dbaf787a434283
Otar Chekurishvili <otar@chekurishvili.com>
2014-02-09 20:26:11 +0400

    Issue #142 - New package maintainers will be notified by email.

--- src/Packagist/WebBundle/Model/PackageManager.php
+++ src/Packagist/WebBundle/Model/PackageManager.php
@@ -76,6 +76,32 @@ class PackageManager
             $this->em->flush();
         }
 
+
         return true;
     }
+
+    public function notifyNewMaintainer($user, $package)
+    {
+        $body = $this->twig->render('PackagistWebBundle:Email:maintainer_added.txt.twig', array(
+            'package_name' => $package->getName()
+        ));
+echo $user->getEmail();
+        $message = \Swift_Message::newInstance()
+            ->setSubject('You\'ve been added to ' . $package->getName() . ' as a maintainer')
+            ->setFrom($this->options['from'], $this->options['fromName'])
+            ->setTo($user->getEmail())
+            ->setBody($body)
+        ;
+
+        try {
+            $this->mailer->send($message);
+        } catch (\Swift_TransportException $e) {
+            $this->logger->err('['.get_class($e).'] '.$e->getMessage());
+
+            return FALSE;
+        }
+
+        return TRUE;
+    }
+
 }
