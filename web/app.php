becd028d1bd5719757185443be3ff0506ded5c8b
Jordi Boggiano <j.boggiano@seld.be>
2016-07-13 14:36:46 +0100

    Remove usage of apc class loader

--- web/app.php
+++ web/app.php
@@ -2,7 +2,6 @@
 
 ini_set('date.timezone', 'UTC');
 
-use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -10,13 +9,6 @@ use Symfony\Component\HttpFoundation\Request;
  */
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../app/bootstrap.php.cache';
-
-// Use APC for autoloading to improve performance.
-if (function_exists('apc_store')) {
-    $apcLoader = new ApcClassLoader(sha1(__FILE__), $loader);
-    $loader->unregister();
-    $apcLoader->register(true);
-}
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
