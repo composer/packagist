1c196a10368d28a677998fcf48b1b0ae1c1deb3b
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-10-23 21:06:18 +0200

    Added tests for packages that always should fail

--- src/Packagist/WebBundle/Tests/Entity/PackageTest.php
+++ src/Packagist/WebBundle/Tests/Entity/PackageTest.php
@@ -7,6 +7,7 @@ use Packagist\WebBundle\Entity\Package;
 use Prophecy\Argument;
 use ReflectionObject;
 use Symfony\Component\Validator\Context\ExecutionContextInterface;
+use Symfony\Component\Validator\Violation\ConstraintViolationBuilderInterface;
 
 class PackageTest extends \PHPUnit_Framework_TestCase
 {
@@ -29,25 +30,46 @@ class PackageTest extends \PHPUnit_Framework_TestCase
         ];
 
         foreach ($packages as $packageName) {
-            $package = new Package();
-            $package->setName($packageName);
-
-            $vcsDriver = $this->prophesize(VcsDriverInterface::class);
-            $vcsDriver->getComposerInformation('master')->shouldBeCalled()->willReturn([
-                'name' => $packageName,
-            ]);
-            $vcsDriver->getRootIdentifier()->shouldBeCalled()->willReturn('master');
-
-
-            $r = new ReflectionObject($package);
-            $p = $r->getProperty('vcsDriver');
-            $p->setAccessible(true);
-            $p->setValue($package, $vcsDriver->reveal());
-
             yield $packageName => [
-                $package,
+                $this->createPackage($packageName),
             ];
         }
+    }
+
+    public function provideInvalidPackages()
+    {
+        $invalidPackageNameMessage = 'The package name %s is invalid, it should have a vendor name, a forward slash, and a package name. The vendor and package name can be words separated by -, . or _. The complete name should match "[a-z0-9]([_.-]?[a-z0-9]+)*/[a-z0-9]([_.-]?[a-z0-9]+)*".';
+        $packages = [
+            'CVE-2006-6459' => sprintf($invalidPackageNameMessage, 'CVE-2006-6459'),
+            '☼/🔥' => sprintf($invalidPackageNameMessage, '☼/🔥'),
+            './.' => sprintf($invalidPackageNameMessage, './.'),
+        ];
+
+        foreach ($packages as $packageName => $expectedException) {
+            yield $packageName => [
+                $this->createPackage($packageName),
+                $expectedException
+            ];
+        }
+    }
+
+    protected function createPackage(string $packageName): Package
+    {
+        $package = new Package();
+        $package->setName($packageName);
+
+        $vcsDriver = $this->prophesize(VcsDriverInterface::class);
+        $vcsDriver->getComposerInformation('master')->shouldBeCalled()->willReturn([
+            'name' => $packageName,
+        ]);
+        $vcsDriver->getRootIdentifier()->shouldBeCalled()->willReturn('master');
+
+        $r = new ReflectionObject($package);
+        $p = $r->getProperty('vcsDriver');
+        $p->setAccessible(true);
+        $p->setValue($package, $vcsDriver->reveal());
+
+        return $package;
     }
 
     /**
@@ -58,5 +80,20 @@ class PackageTest extends \PHPUnit_Framework_TestCase
         $executionContext = $this->prophesize(ExecutionContextInterface::class);
         $package->isRepositoryValid($executionContext->reveal());
         $this->assertTrue(true, 'No exception occurred');
+    }
+
+    /**
+     * @dataProvider provideInvalidPackages
+     */
+    public function testIsRepositoryInvalid(Package $package, string $exceptionMessage)
+    {
+        $executionContextLevel3 = $this->prophesize(ConstraintViolationBuilderInterface::class);
+
+        $executionContextLevel2 = $this->prophesize(ConstraintViolationBuilderInterface::class);
+        $executionContextLevel2->atPath(Argument::any())->willReturn($executionContextLevel3->reveal());
+
+        $executionContext = $this->prophesize(ExecutionContextInterface::class);
+        $executionContext->buildViolation($exceptionMessage)->shouldBeCalled()->willReturn($executionContextLevel2->reveal());
+        $package->isRepositoryValid($executionContext->reveal());
     }
 }
