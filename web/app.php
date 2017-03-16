7948968294ff642ff974412d8b1398fb2ab7cdc4
Jordi Boggiano <j.boggiano@seld.be>
2017-03-16 11:51:04 +0100

    Add support for proxy

--- web/app.php
+++ web/app.php
@@ -10,6 +10,17 @@ include_once __DIR__.'/../app/bootstrap.php.cache';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
+
+if ($_SERVER['REMOTE_ADDR'] === '144.217.203.53') {
+    Request::setTrustedProxies([$_SERVER['REMOTE_ADDR']]);
+    // force all trusted header names
+    Request::setTrustedHeaderName(Request::HEADER_FORWARDED, '');
+    Request::setTrustedHeaderName(Request::HEADER_CLIENT_IP, 'X_REAL_IP');
+    Request::setTrustedHeaderName(Request::HEADER_CLIENT_HOST, '');
+    Request::setTrustedHeaderName(Request::HEADER_CLIENT_PROTO, '');
+    Request::setTrustedHeaderName(Request::HEADER_CLIENT_PORT, '');
+}
+
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
