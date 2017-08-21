cae3f11c0e325fd4d294adfb692e9620d89abb09
Julien Bourdeau <julien@sigerr.org>
2017-08-21 22:27:08 +0000

    Pass Algolia config to JS

--- src/Packagist/WebBundle/Resources/public/js/search.js
+++ src/Packagist/WebBundle/Resources/public/js/search.js
@@ -1,7 +1,7 @@
 var search = instantsearch({
-    appId: '6B7GQC987O',
-    apiKey: 'a4b2a9870a8575c9e5c3d2dd00d8de95',
-    indexName: 'packagist',
+    appId: algoliaConfig.app_id,
+    apiKey: algoliaConfig.search_key,
+    indexName: algoliaConfig.index_name,
     urlSync: true
 });
 
