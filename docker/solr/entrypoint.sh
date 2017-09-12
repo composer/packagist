b5fd70e88632252bf0635986fd8985d52c11c2bc
Alessandro Lai <alessandro.lai85@gmail.com>
2017-09-12 13:45:39 +0200

    Refactor even more the Solr container

--- docker/solr/entrypoint.sh
+++ docker/solr/entrypoint.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-SOLR_PORT=${SOLR_PORT:-8983}
-SOLR_VERSION=${SOLR_VERSION:-4.9.1}
-DEBUG=${DEBUG:-false}
-SOLR_CORE=${SOLR_CORE:-core0}
-# Since Solr 5.x
-SOLR_COLLECTION=${SOLR_COLLECTION:-gettingstarted}
-
 is_solr_up(){
     echo "Checking if solr is up on http://localhost:$SOLR_PORT/solr/admin/cores"
     http_code=`echo $(curl -s -o /dev/null -w "%{http_code}" "http://localhost:$SOLR_PORT/solr/admin/cores")`
@@ -48,46 +41,8 @@ run() {
 initialize_and_run() {
 	version='3.6.0'
     dir_name="apache-solr-${version}"
-    dir_conf="conf/"
-    add_core $dir_name $dir_conf $SOLR_CORE "$SOLR_CONFS"
     run $dir_name $SOLR_PORT $SOLR_CORE
     echo "skipping initial indexing"
-}
-
-add_core() {
-    dir_name=$1
-    dir_conf=$2
-    solr_core=$3
-    solr_confs=$4
-    # prepare our folders
-    [[ -d "${dir_name}/example/multicore/${solr_core}" ]] || mkdir $dir_name/example/multicore/$solr_core
-    [[ -d "${dir_name}/example/multicore/${solr_core}/conf" ]] || mkdir $dir_name/example/multicore/$solr_core/conf
-
-    # copy text configs from default single core conf to new core to have proper defaults
-    cp -R $dir_name/example/solr/conf/{lang,*.txt} $dir_name/example/multicore/$solr_core/conf/
-
-    # copies custom configurations
-    if [ -d "${solr_confs}" ] ; then
-      cp -R $solr_confs/* $dir_name/example/multicore/$solr_core/conf/
-      echo "Copied $solr_confs/* to solr conf directory."
-    else
-      for file in $solr_confs
-      do
-        if [ -f "${file}" ]; then
-            cp $file $dir_name/example/multicore/$solr_core/conf
-            echo "Copied $file into solr conf directory."
-        else
-            echo "${file} is not valid";
-            exit 1
-        fi
-      done
-    fi
-
-    # enable custom core
-    if [ "$solr_core" != "core0" -a "$solr_core" != "core1" ] ; then
-        echo "Adding $solr_core to solr.xml"
-        sed -i -e "s/<\/cores>/<core name=\"$solr_core\" instanceDir=\"$solr_core\" \/><\/cores>/" $dir_name/example/multicore/solr.xml
-    fi
 }
 
 initialize_and_run
