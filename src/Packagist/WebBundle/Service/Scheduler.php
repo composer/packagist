6a339a36965f39133877dffe80b38afe20e825df
Christophe Coevoet <stof@notk.org>
2018-03-08 11:38:03 +0100

    Fix tests

--- src/Packagist/WebBundle/Service/Scheduler.php
+++ src/Packagist/WebBundle/Service/Scheduler.php
@@ -5,7 +5,6 @@ namespace Packagist\WebBundle\Service;
 use Doctrine\Common\Persistence\ManagerRegistry;
 use Predis\Client as RedisClient;
 use Packagist\WebBundle\Entity\Package;
-use Packagist\WebBundle\Entity\Organization;
 use Packagist\WebBundle\Entity\Job;
 
 class Scheduler
