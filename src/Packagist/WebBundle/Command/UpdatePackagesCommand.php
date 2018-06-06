bf4a31189a7fa4bf9f66271361c79507a64a869c
Jordi Boggiano <j.boggiano@seld.be>
2018-06-06 16:11:45 +0200

    Spread package updates in a lesser time space so the command can be run more frequently

--- src/Packagist/WebBundle/Command/UpdatePackagesCommand.php
+++ src/Packagist/WebBundle/Command/UpdatePackagesCommand.php
@@ -102,7 +102,7 @@ class UpdatePackagesCommand extends ContainerAwareCommand
             $idsGroup = array_splice($ids, 0, 100);
 
             foreach ($idsGroup as $id) {
-                $job = $scheduler->scheduleUpdate($id, $updateEqualRefs, $deleteBefore, $randomTimes ? new \DateTime('+'.rand(1, 1800).'seconds') : null);
+                $job = $scheduler->scheduleUpdate($id, $updateEqualRefs, $deleteBefore, $randomTimes ? new \DateTime('+'.rand(1, 600).'seconds') : null);
                 if ($verbose) {
                     $output->writeln('Scheduled update job '.$job->getId().' for package '.$id);
                 }
