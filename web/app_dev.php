aafb7772d02737e82fd3b692260a169162e1aa18
Koala Yeung <koalay@gmail.com>
2018-12-14 15:42:53 +0800

    Add PHP test server router script support
    * Detects asset requiest uri, when called, and return false.
      Allows `web/app_dev.php` to function as a PHP test server router
      script.

--- web/app_dev.php
+++ web/app_dev.php
@@ -10,6 +10,15 @@ use Symfony\Component\Debug\Debug;
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
 //umask(0000);
 
+// When used directly as a php router script,
+// allow all assets to be served normally.
+if (preg_match(
+      '/\.(?:css|eot|gif|ico|js|osd|png|svg|ttf|txt|woff|woff2)$/i',
+      parse_url($_SERVER['REQUEST_URI'])['path'] ?? '')
+) {
+    return false;
+}
+
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
