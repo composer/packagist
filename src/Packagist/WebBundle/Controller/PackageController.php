1d33b9de2c2140e1fe7956ef02fe468812db7cc9
Jordi Boggiano <j.boggiano@seld.be>
2020-03-25 20:41:58 +0100

    Fix issue with filemtime discrepancy

--- src/Packagist/WebBundle/Controller/PackageController.php
+++ src/Packagist/WebBundle/Controller/PackageController.php
@@ -130,7 +130,8 @@ class PackageController extends Controller
 
         $actions = [];
         foreach ($dumps as $package => $time) {
-            $actions[$package] = ['type' => 'update', 'package' => $package, 'time' => floor($time / 10000)];
+            // we subtract 1s to the time for safety, as filemtime can be a little before the logged time (see https://gist.github.com/Seldaek/a5cf4a5551139bc41fb130fa16406290)
+            $actions[$package] = ['type' => 'update', 'package' => $package, 'time' => floor($time / 10000) - 1];
         }
         foreach ($deletes as $package => $time) {
             // if a package is dumped then deleted then dumped again because it gets re-added, we want to keep the update action
@@ -138,7 +139,7 @@ class PackageController extends Controller
             // dumped job and deletion, so let's replace it by a delete job anyway
             $newestUpdate = max($actions[$package]['time'] ?? 0, $actions[$package.'~dev']['time'] ?? 0);
             if ($newestUpdate < $time + (10 * 10000)) {
-                $actions[$package] = ['type' => 'delete', 'package' => $package, 'time' => floor($time / 10000)];
+                $actions[$package] = ['type' => 'delete', 'package' => $package, 'time' => floor($time / 10000) - 1];
                 unset($actions[$package.'~dev']);
             }
         }
