b7c8d8d039bda49b287779c089d885c2f9ebcab3
Jordi Boggiano <j.boggiano@seld.be>
2020-11-24 10:23:33 +0100

    Remove generator bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -32,7 +32,6 @@ class AppKernel extends Kernel
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
 
         return $bundles;
