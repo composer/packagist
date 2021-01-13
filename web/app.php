ad7e3c675a4a3a8876f5dda822284c33cda7fc45
Jordi Boggiano <j.boggiano@seld.be>
2021-01-13 14:09:06 +0100

    Final tweaks

--- web/app.php
+++ web/app.php
@@ -22,6 +22,14 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
     Request::setTrustedHosts([$trustedHosts]);
 }
 
+Request::setTrustedProxies(
+    // remote_addr is set to the correct client IP but we need to mark it trusted so that Symfony picks up the X-Forwarded-Host,
+    // X-Forwarded-Port and X-Forwarded-Proto headers correctly and sees the right request URL
+    [$_SERVER['REMOTE_ADDR']],
+    // Use all X-Forwarded-* headers except X-Forwarded-For as nginx handles the IP computation
+    Request::HEADER_X_FORWARDED_AWS_ELB ^ Request::HEADER_X_FORWARDED_FOR
+);
+
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
