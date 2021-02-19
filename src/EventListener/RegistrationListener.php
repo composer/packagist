bb5d319ed2bb6a08fd8de3113985cff8a37fa479
Benjamin Morel <benjamin.morel@gmail.com>
2021-02-19 12:58:00 +0100

    Use TokenGeneratorInterface

--- src/EventListener/RegistrationListener.php
+++ src/EventListener/RegistrationListener.php
@@ -4,7 +4,7 @@ namespace App\EventListener;
 
 use FOS\UserBundle\Event\FormEvent;
 use FOS\UserBundle\FOSUserEvents;
-use FOS\UserBundle\Util\TokenGenerator;
+use FOS\UserBundle\Util\TokenGeneratorInterface;
 use App\Entity\User;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
@@ -13,12 +13,12 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
  */
 class RegistrationListener implements EventSubscriberInterface
 {
-    private TokenGenerator $tokenGenerator;
+    private TokenGeneratorInterface $tokenGenerator;
 
     /**
-     * @param TokenGenerator $tokenGenerator
+     * @param TokenGeneratorInterface $tokenGenerator
      */
-    public function __construct(TokenGenerator $tokenGenerator)
+    public function __construct(TokenGeneratorInterface $tokenGenerator)
     {
         $this->tokenGenerator = $tokenGenerator;
     }
