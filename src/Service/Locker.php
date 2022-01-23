e82ed1824a9f662378a79a8517341b4017edb430
Christopher Hertel <mail@christopher-hertel.de>
2022-01-23 14:11:03 +0100

    Connect to primary if replica

--- src/Service/Locker.php
+++ src/Service/Locker.php
@@ -3,6 +3,7 @@
 namespace App\Service;
 
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Connections\PrimaryReadReplicaConnection;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Util\DoctrineTrait;
 
@@ -16,21 +17,21 @@ class Locker
 
     public function lockPackageUpdate(int $packageId, int $timeout = 0): bool
     {
-        $this->getConn()->connect();
+        $this->connect();
 
         return (bool) $this->getConn()->fetchOne('SELECT GET_LOCK(:id, :timeout)', ['id' => 'package_update_'.$packageId, 'timeout' => $timeout]);
     }
 
     public function unlockPackageUpdate(int $packageId): void
     {
-        $this->getConn()->connect();
+        $this->connect();
 
         $this->getConn()->fetchOne('SELECT RELEASE_LOCK(:id)', ['id' => 'package_update_'.$packageId]);
     }
 
     public function lockSecurityAdvisory(string $source, int $timeout = 0): bool
     {
-        $this->getConn()->connect();
+        $this->connect();
 
         return (bool) $this->getConn()->fetchOne('SELECT GET_LOCK(:id, :timeout)', ['id' => 'security_advisory_'.$source, 'timeout' => $timeout]);
     }
@@ -44,7 +45,7 @@ class Locker
 
     public function lockCommand(string $command, int $timeout = 0): bool
     {
-        $this->getConn()->connect();
+        $this->connect();
 
         return (bool) $this->getConn()->fetchOne(
             'SELECT GET_LOCK(:id, :timeout)',
@@ -54,12 +55,20 @@ class Locker
 
     public function unlockCommand(string $command): void
     {
-        $this->getConn()->connect();
+        $this->connect();
 
         $this->getConn()->fetchOne(
             'SELECT RELEASE_LOCK(:id)',
             ['id' => $command]
         );
+    }
+
+    private function connect(): void
+    {
+        $connection = $this->getConn();
+        if ($connection instanceof PrimaryReadReplicaConnection) {
+            $connection->connect('primary');
+        }
     }
 
     private function getConn(): Connection
