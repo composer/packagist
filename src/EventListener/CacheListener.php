62f8e0281f5033c8b7274233ef76fade71354d79
Christopher Hertel <mail@christopher-hertel.de>
2022-01-23 13:01:39 +0100

    Fixing EventListener namespace for PhpStan

--- src/EventListener/CacheListener.php
+++ src/EventListener/CacheListener.php
@@ -6,13 +6,13 @@ use Symfony\Component\HttpKernel\Event\ResponseEvent;
 
 class CacheListener
 {
-    public function onResponse(ResponseEvent $e)
+    public function onResponse(ResponseEvent $e): void
     {
         $resp = $e->getResponse();
 
         // add nginx-cache compatible header
         if ($resp->headers->hasCacheControlDirective('public') && ($cache = $resp->headers->getCacheControlDirective('s-maxage'))) {
-            $resp->headers->set('X-Accel-Expires', $cache);
+            $resp->headers->set('X-Accel-Expires', (string)$cache);
         }
     }
 }
