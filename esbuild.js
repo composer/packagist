b9d26a2411f9a40980285e7b4f0841163651656c
Christopher Hertel <mail@christopher-hertel.de>
2022-02-21 18:29:16 +0100

    Introduce esbuild build script

---
+++ esbuild.js
@@ -0,0 +1,21 @@
+const esbuild = require('esbuild');
+
+esbuild.build({
+    logLevel: 'info',
+    entryPoints: ['js/app.js', 'js/charts.js'],
+    bundle: true,
+    outdir: 'web/build',
+    sourcemap: process.argv.includes('--dev'),
+    watch: process.argv.includes('--dev'),
+    minify: !process.argv.includes('--dev'),
+    loader: {
+        '.gif':'file',
+        '.eot':'file',
+        '.ttf':'file',
+        '.svg':'file',
+        '.woff':'file',
+        '.woff2':'file',
+    },
+    target: ['chrome58', 'firefox57', 'safari11', 'edge95'],
+})
+.catch(() => process.exit(1));
