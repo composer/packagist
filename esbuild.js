29525887af9dcd4360e97d7bf50d3d1417e494b2
Christopher Hertel <mail@christopher-hertel.de>
2022-04-02 14:26:44 +0200

    readding esbuild analyze capabilities

--- esbuild.js
+++ esbuild.js
@@ -1,23 +1,31 @@
-const esbuild = require('esbuild');
-const sassPlugin = require('esbuild-plugin-sass');
+(async () => {
+    const esbuild = require('esbuild');
+    const sassPlugin = require('esbuild-plugin-sass');
 
-esbuild.build({
-    logLevel: 'info',
-    entryPoints: ['js/app.js', 'js/charts.js'],
-    bundle: true,
-    outdir: 'web/build',
-    sourcemap: process.argv.includes('--dev'),
-    watch: process.argv.includes('--dev'),
-    minify: !process.argv.includes('--dev'),
-    loader: {
-        '.gif':'file',
-        '.eot':'file',
-        '.ttf':'file',
-        '.svg':'file',
-        '.woff':'file',
-        '.woff2':'file',
-    },
-    target: ['chrome58', 'firefox57', 'safari11', 'edge95'],
-    plugins: [sassPlugin()],
-})
-.catch(() => process.exit(1));
+    const result = await esbuild.build({
+        logLevel: 'info',
+        entryPoints: ['js/app.js', 'js/charts.js'],
+        bundle: true,
+        outdir: 'web/build',
+        sourcemap: process.argv.includes('--dev'),
+        watch: process.argv.includes('--dev'),
+        minify: !process.argv.includes('--dev'),
+        metafile: process.argv.includes('--analyze'),
+        loader: {
+            '.gif':'file',
+            '.eot':'file',
+            '.ttf':'file',
+            '.svg':'file',
+            '.woff':'file',
+            '.woff2':'file',
+        },
+        target: ['chrome58', 'firefox57', 'safari11', 'edge95'],
+        plugins: [sassPlugin()],
+    })
+    .catch(() => process.exit(1));
+
+    if (process.argv.includes('--analyze')) {
+        const text = await esbuild.analyzeMetafile(result.metafile)
+        console.log(text);
+    }
+})()
