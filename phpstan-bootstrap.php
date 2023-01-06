999b411ff861fa1397d7cd9b22c675e2a36779dd
Markus Staab <maggus.staab@googlemail.com>
2023-01-06 21:04:54 +0100

    phpstan-dba: enable analysis of writable queries

--- phpstan-bootstrap.php
+++ phpstan-bootstrap.php
@@ -14,6 +14,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 $cacheFile = __DIR__.'/.phpstan-dba.cache';
 $config = new RuntimeConfiguration();
 $config->stringifyTypes(false);
+$config->analyzeWriteQueries(true);
 // $config->analyzeQueryPlans(true);
 // $config->debugMode(true);
 
