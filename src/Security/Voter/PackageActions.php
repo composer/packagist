022fa72a583e86266922edf29241100fab887d19
Jordi Boggiano <j.boggiano@seld.be>
2023-05-02 14:47:28 +0200

    Refactor package permissions into a voter, restrict URL edits from 100K dls

---
+++ src/Security/Voter/PackageActions.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Security\Voter;
+
+enum PackageActions: string
+{
+    case Edit = 'edit';
+    case AddMaintainer = 'add_maintainer';
+    case RemoveMaintainer = 'remove_maintainer';
+    case Abandon = 'abandon';
+    case Unabandon = 'unabandon';
+    case Delete = 'delete';
+    case Update = 'update';
+}
---
+++ src/Security/Voter/PackageVoter.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\Package;
+use App\Entity\User;
+use App\Model\DownloadManager;
+use Predis\Connection\ConnectionException;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+class PackageVoter extends \Symfony\Component\Security\Core\Authorization\Voter\Voter
+{
+    public function __construct(
+        private Security $security,
+        private DownloadManager $downloadManager,
+    ) {
+    }
+
+    protected function supports(string $attribute, mixed $subject): bool
+    {
+        return $subject instanceof Package && PackageActions::tryFrom($attribute) instanceof PackageActions;
+    }
+
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+
+        // the user must be logged in
+        if (!$user instanceof User) {
+            return false;
+        }
+
+        /** @var Package $package */
+        $package = $subject;
+
+        return match(PackageActions::from($attribute)) {
+            PackageActions::Abandon => !$package->isAbandoned() && ($package->isMaintainer($user) || $this->security->isGranted('ROLE_EDIT_PACKAGES')),
+            PackageActions::Unabandon => $package->isAbandoned() && ($package->isMaintainer($user) || $this->security->isGranted('ROLE_EDIT_PACKAGES')),
+            PackageActions::Delete => $this->canDelete($package, $user),
+            PackageActions::Edit => $this->canEdit($package, $user),
+            PackageActions::AddMaintainer => $this->canAddMaintainers($package, $user),
+            PackageActions::RemoveMaintainer => $this->canRemoveMaintainers($package, $user),
+            PackageActions::Update => $package->isMaintainer($user) || $this->security->isGranted('ROLE_UPDATE_PACKAGES'),
+        };
+    }
+
+    private function canDelete(Package $package, User $user): bool
+    {
+        if ($this->security->isGranted('ROLE_DELETE_PACKAGES')) {
+            return true;
+        }
+
+        // non maintainers can not delete
+        if (!$package->isMaintainer($user)) {
+            return false;
+        }
+
+        try {
+            $downloads = $this->downloadManager->getTotalDownloads($package);
+        } catch (ConnectionException $e) {
+            return false;
+        }
+
+        // more than 1000 downloads = established package, do not allow deletion by maintainers
+        if ($downloads > 1_000) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private function canEdit(Package $package, User $user): bool
+    {
+        if ($this->security->isGranted('ROLE_EDIT_PACKAGES')) {
+            return true;
+        }
+
+        // non maintainers can not edit
+        if (!$package->isMaintainer($user)) {
+            return false;
+        }
+
+        try {
+            $downloads = $this->downloadManager->getTotalDownloads($package);
+        } catch (ConnectionException $e) {
+            return false;
+        }
+
+        // more than 100000 downloads = established package, do not allow editing URL anymore
+        if ($downloads > 100_000) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private function canAddMaintainers(Package $package, User $user): bool
+    {
+        if ($this->security->isGranted('ROLE_EDIT_PACKAGES')) {
+            return true;
+        }
+
+        if (!$package->isMaintainer($user)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private function canRemoveMaintainers(Package $package, User $user): bool
+    {
+        // at least one has to remain
+        if (1 === $package->getMaintainers()->count()) {
+            return false;
+        }
+
+        return $this->canAddMaintainers($package, $user);
+    }
+
+    public function supportsAttribute(string $attribute): bool
+    {
+        return PackageActions::tryFrom($attribute) instanceof PackageActions;
+    }
+
+    public function supportsType(string $subjectType): bool
+    {
+        return $subjectType === Package::class;
+    }
+}
