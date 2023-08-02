abb83bdbd1ada228059bc87d71b0b06ac0937435
Jordi Boggiano <j.boggiano@seld.be>
2023-08-02 18:12:33 +0200

    Add missing line

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -28,6 +28,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Graze\DogStatsD\Client as StatsDClient;
