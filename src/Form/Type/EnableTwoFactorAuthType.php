c8e3a669acabd42021643f6c1f3f6ffd4d09eb0f
Stephan Vock <stephan.vock@gmail.com>
2023-11-17 13:22:57 +0000

    Two-Factor: extract code validation into constraint and add test to enable two-factor

--- src/Form/Type/EnableTwoFactorAuthType.php
+++ src/Form/Type/EnableTwoFactorAuthType.php
@@ -12,7 +12,9 @@
 
 namespace App\Form\Type;
 
+use App\Entity\User;
 use App\Form\Model\EnableTwoFactorRequest;
+use App\Form\Validation\TwoFactorCodeConstraint;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -25,13 +27,18 @@ class EnableTwoFactorAuthType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder->add('code', TextType::class);
+        $builder->add('code', TextType::class, [
+            'constraints' => [new TwoFactorCodeConstraint($options['user'])]
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
-        $resolver->setDefaults([
-            'data_class' => EnableTwoFactorRequest::class,
-        ]);
+        $resolver
+            ->setDefaults([
+                'data_class' => EnableTwoFactorRequest::class,
+                'user' => null,
+            ])
+            ->setAllowedTypes('user', User::class);
     }
 }
---
+++ src/Form/Validation/TwoFactorCodeConstraintValidator.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of Packagist.
+ *
+ * (c) Jordi Boggiano <j.boggiano@seld.be>
+ *     Nils Adermann <naderman@naderman.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Form\Validation;
+
+use Scheb\TwoFactorBundle\Security\TwoFactor\Provider\Totp\TotpAuthenticatorInterface;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class TwoFactorCodeConstraintValidator extends ConstraintValidator
+{
+    public function __construct(
+        private readonly TotpAuthenticatorInterface $totpAuthenticator,
+    ) {}
+
+    /**
+     * @param TwoFactorCodeConstraint $constraint
+     */
+    public function validate(mixed $value, Constraint $constraint): void
+    {
+        if (!$this->totpAuthenticator->checkCode($constraint->user, (string) $value)) {
+            $this->context->addViolation($constraint->message);
+        }
+    }
+}
