18b59e1b42abd2ad922a92a64e11420d895ad597
Christopher Hertel <mail@christopher-hertel.de>
2024-03-08 12:02:28 +0100

    recipes update symfony/security-bundle

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -29,13 +29,4 @@ class SecurityController extends Controller
             'error' => $error,
         ]);
     }
-
-    /**
-     * @phpstan-return never
-     */
-    #[Route(path: '/logout', name: 'logout')]
-    public function logout(): void
-    {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-    }
 }
