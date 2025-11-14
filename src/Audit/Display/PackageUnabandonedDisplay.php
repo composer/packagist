e32830fd950d12fe0400fc01ad6e83df7def3d9c
Igor Benko <i.benko@packagist.com>
2025-11-14 17:52:19 +0100

    Track abandoned and unabandoned events in transparency log

---
+++ src/Audit/Display/PackageUnabandonedDisplay.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of Packagist.
+ *
+ * (c) Jordi Boggiano <j.boggiano@seld.be>
+ *     Nils Adermann <naderman@naderman.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Audit\Display;
+
+use App\Audit\AuditRecordType;
+
+readonly class PackageUnabandonedDisplay extends AbstractAuditLogDisplay
+{
+    public function __construct(
+        \DateTimeImmutable $datetime,
+        public string $packageName,
+        public string $repository,
+        public ?string $previousReplacementPackage,
+        ActorDisplay $actor,
+    ) {
+        parent::__construct($datetime, $actor);
+    }
+
+    public function getType(): AuditRecordType
+    {
+        return AuditRecordType::PackageUnabandoned;
+    }
+
+    public function getTemplateName(): string
+    {
+        return 'audit_log/display/package_unabandoned.html.twig';
+    }
+}
