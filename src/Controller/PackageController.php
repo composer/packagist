c7ca60ca8f773394851c6875200d9b31449cfe7d
Igor Benko <i.benko@packagist.com>
2026-09-02 15:49:18 +0200

    Fix tests

--- src/Controller/PackageController.php
+++ src/Controller/PackageController.php
@@ -62,6 +62,7 @@ use Composer\Semver\Constraint\MultiConstraint;
 use Doctrine\DBAL\Exception\DriverException;
 use Doctrine\ORM\NoResultException;
 use Pagerfanta\Adapter\FixedAdapter;
+use Pagerfanta\Doctrine\ORM\QueryAdapter;
 use Pagerfanta\Pagerfanta;
 use Predis\Client as RedisClient;
 use Predis\PredisException;
